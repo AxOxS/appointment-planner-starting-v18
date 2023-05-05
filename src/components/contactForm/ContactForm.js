dbf273118d427c627df010b671048ee3a3d9e925
AxOxS <axoxs1@gmail.com>
2023-05-05 12:05:48 +0300

    ContactForm

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Form } from "react-router-dom";
 
 export const ContactForm = ({
   name,
@@ -10,7 +11,46 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+      <input 
+        type="text" 
+        name="name" 
+        value={name} 
+        onChange={(e) => setName(e.target.value)}
+        required
+        placeholder="Contact Name"
+        aria-label="Contact Name"
+      />
+      </label>
+      <br/>
+      <label>
+      <input 
+        type="tel" 
+        name="phone" 
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        pattern="(86|\+3706)\d{3}\d{4}"
+        required
+        placeholder="Contact Phone (+370/8)########"
+        aria-label="Contact Phone"
+      />
+      </label>
+      <br/>
+      <label>
+      <input 
+        type="email" 
+        name="email" 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        placeholder="Contact Email"
+        aria-label="Contact Email"
+      />
+      </label>
+      <br/>
+      <input type="submit" value={"Add Contact"} aria-label="Add Contact"/>
+    </form>
   );
 };
 
